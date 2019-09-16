ee30b22fe0f90bdb50bd08c569c508c825b48c79
Josue Tchirktema <tchirktemajosue@gmail.com>
2019-09-16 18:01:27 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {sampleText} from './sampleText'
+import marked from 'marked'
+import sanitize from 'sanitize-html'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends Component {
+  state = {
+    text : sampleText
+  }
+
+  handleChange = (event)=>{
+    const text = event.target.value
+    this.setState({text})
+  }
+
+  renderText = text => {
+    const __html = marked(sanitize(text))
+
+    return {__html}
+  }
+
+  componentDidMount(){
+    
+    const text = localStorage.getItem('text')
+    if(text){
+      this.setState({text})
+    }else{
+      this.setState({ text: sampleText})
+    }
+    
+  }
+  componentDidUpdate(){
+    const { text } = this.state
+    localStorage.setItem('text',text)
+  }
+
+  render(){
+    return (
+      <div className="container">
+          <div className="row">
+              <div className="col-sm-6">
+                  <textarea
+                    onChange={this.handleChange}
+                    value={this.state.text}
+                    className="form-control"
+                    rows="25"
+                    >
+  
+                  </textarea>
+              </div>
+              <div className="col-sm-6">
+                  <div dangerouslySetInnerHTML={this.renderText(this.state.text)}>
+                    
+                  </div>
+              </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
